afbba9eaa98b152ea8bb644a958f27cb852bd01e
Jackson Pires <jack@jack.com>
2020-03-10 20:25:16 +0000

    Corrects system and adjusts layout. profile model changed to user

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  namespace :profiles_backoffice do
+  namespace :users_backoffice do
     get 'welcome/index'
   end
-  devise_for :profiles
+  
+  devise_for :users
   devise_for :admins
+
   get 'inicio', to:'site/welcome#index'
 
   root to: 'site/welcome#index'
