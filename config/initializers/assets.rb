2d5689a730cec2ec63bfc4cdf916b6164f7f26e1
Vagner Oliveira <ti.vagner@gmail.com>
2020-03-11 16:29:07 -0300

    working layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,7 +11,21 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
+
+#/app/assets
+Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css 
+                                                  users_backoffice.js users_backoffice.css )
+
+# /lib/assets
+Rails.application.config.assets.precompile += %w( sb-admin2.min.js sb-admin-2.min.css
+                                                  custom.min.js custom.min.css 
+                                                  img.jpg)
 
 
-Rails.application.config.assets.precompile += %w( sb-admin2.min.js sb-admin-2.min.css )
+Rails.application.config.assets.precompile += %w(jquery-2.2.3/dist/jquery.min.js)
+
+
+
+
+
+                                                  
