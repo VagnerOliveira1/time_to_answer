f48f5ba9d2bac57561381c37e1a8aa1b55290692
Vagner Oliveira <ti.vagner@gmail.com>
2020-03-15 08:24:46 -0300

    research with scope
    https://api.rubyonrails.org/classes/ActiveRecord/Scoping/Named/ClassMethods.html

--- app/controllers/site/welcome_controller.rb
+++ app/controllers/site/welcome_controller.rb
@@ -1,6 +1,6 @@
 class Site::WelcomeController < SiteController
   layout 'site'
   def index
-    @questions = Question.includes(:answers).page(params[:page])
+    @questions = Question.last_questions(params[:page])
   end
 end
